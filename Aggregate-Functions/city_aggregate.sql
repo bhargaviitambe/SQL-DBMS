c2f074963d70be26b73ce732947a0659971b19b9
Bhargavi Tambe <bhargavi.tambe19@gmail.com>
2026-07-05 22:00:11 +0530

    Added in aggregate functions city table queries

--- Aggregate-Functions/city_aggregate.sql
+++ Aggregate-Functions/city_aggregate.sql
@@ -2,3 +2,4 @@ select count(NAME) from CITY where POPULATION>100000;
 select sum(POPULATION) from CITY where DISTRICT='California';
 select avg(POPULATION) from CITY where DISTRICT='California';
 select round(avg(POPULATION), 0) from CITY;
+SELECT SUM(POPULATION) FROM CITY WHERE COUNTRYCODE='JPN';
