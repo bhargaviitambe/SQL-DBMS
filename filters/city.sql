5432fc604c14aa3fa2afa5144fdc9a6f10d45239
Bhargavi Tambe <bhargavi.tambe19@gmail.com>
2026-07-15 22:59:59 +0530

    added

--- filters/city.sql
+++ filters/city.sql
@@ -1,2 +1,3 @@
 SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '[AEIOU]$';
 SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) NOT IN ('A','E','I','O','U','a','e','i','o','u') OR RIGHT(CITY, 1) NOT IN ('A','E','I','O','U','a','e','i','o','u');
